ef2d5f291004e40901c2d4bd86d7108b5d56d044
kiimolliveiraa <kiimolliveiraa@gmail.com>
2023-04-08 00:44:28 -0300

    Primeiro commint

---
+++ src/App.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class App {
+
+    public static void main (String[]args) {
+
+        double centimetro;
+        double metro;
+
+        try (Scanner teclado = new Scanner (System.in)) {
+            
+                
+                System.out.println("Valor de metro:");
+                
+                metro = teclado.nextDouble();
+
+
+                centimetro = metro * 100;
+
+                System.out.printf("O valor convertido para centimetros é:%.1f", centimetro);
+
+            }
+        }
+
+    }
